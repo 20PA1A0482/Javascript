1dbcd8119e9845268b4b05467c7750b0a31caa48
20PA1A0482 <123355536+20PA1A0482@users.noreply.github.com>
2023-06-22 20:50:03 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,10 @@
+function calculateArea(length, width) {
+  var area = length * width;
+  return area;
+}
+
+var length = 10;
+var width = 5;
+var rectangleArea = calculateArea(length, width);
+
+console.log(rectangleArea); // Output: 50
